2614d78b9f984e4b6af6bb3a7b97e16096e8ba62
Robert Henderson <robert@nterpryz.com>
2018-01-10 15:54:14 +0000

    can check if player1 & player2 wins, loses or draws

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,28 @@
+import java.util.HashMap;
+
 public class Game {
+
+    HashMap<Player, String> outcome;
+
+    public Game(){
+        outcome = new HashMap<>();
+    }
+
+    public HashMap<Player,String> decideResult(Player player1, Player player2) {
+
+        if (player1.getHandValue() == player2.getHandValue()){
+            this.outcome.put(player1, "Draw");
+            this.outcome.put(player2, "Draw");
+        } else if (player1.getHandValue() > player2.getHandValue()){
+            this.outcome.put(player1, "Win");
+            this.outcome.put(player2, "Lose");
+        } else {
+            this.outcome.put(player1, "Lose");
+            this.outcome.put(player2, "Win");
+        }
+
+        return outcome;
+    }
 
 
 }
